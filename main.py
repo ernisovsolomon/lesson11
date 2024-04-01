5764e2711c1805fb37b43b0191a0302766be71a9
Сулайман Эрнисов <sulajmanernisov@Sulajmans-MacBook-Air.local>
2024-04-01 19:57:56 +0600

    finished 3 tasks

--- main.py
+++ main.py
@@ -4,11 +4,23 @@
 '''
 Задача 1
 '''
-cities = ['Москва', 'Bishkek', 'Алмата', 'Berlin', 'Ankara', 'Amsterdam']
+# cities = ['Москва', 'Bishkek', 'Алмата', 'Berlin', 'Ankara', 'Amsterdam']
 
-def make_to_lower(city_name):
-    return city_name.lower()
+# # def make_to_lower(city_name):
+# #     return city_name.lower()
+# # cities_lower = list(map(make_to_lower, cities))
+# # print(cities_lower)
 
-cities_lower = list(map(make_to_lower, cities))
+#     cities_lower = list(map(lambda city_name.lower(, cities)))
+#     print(cities_lower)
 
-print(cities_lower)
+'''
+Задача 2
+'''
+# my_list = [4, 3, 6, 8, 9, 7]
+
+# filter(lambda x: )
+
+'''
+Задача 3
+'''
